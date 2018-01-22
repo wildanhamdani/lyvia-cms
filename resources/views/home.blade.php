dd0006b682fa27b2adf8c507c9ee56a9d3451874
Wildan Hamdani <wildan.hamdani@gmail.com>
2018-01-23 04:42:01 +0700

    Init dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,92 @@
 @section('title', 'LYVIA')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>LYVIA ARBITRAGE DASHBOARD</h1>
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="small-box bg-blue">
+                <div class="inner">
+                    <h1>{{\Illuminate\Support\Facades\Auth::user()->name}}</h1>
+                    <h2>Admin</h2>
+
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                    <p>Total Profit</p>
+                    <br>
+                    <br>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-home"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+            <div class="col-lg-6 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                        <h3>150</h3>
+
+                        <p>Jobs</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                        <p>Today Profit</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                        <p>MTD Profit</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                    <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                        <p>YTD Profit</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
 @stop
